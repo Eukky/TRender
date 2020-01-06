118cdfed0dd8291b65ceee5580b1a0714933d44f
Eukky <uki.sept@gmail.com>
2020-01-07 00:18:39 +0800

    update key event

---
+++ RenderCore/Event/KeyPressedEvent.h
@@ -0,0 +1,39 @@
+#ifndef _TRENDER_EVENT_KEYPRESSEDEVENT_H_
+#define _TRENDER_EVENT_KEYPRESSEDEVENT_H_
+
+#include <string>
+#include "KeyEvent.h"
+
+namespace TRender {
+    namespace Event {
+        class KeyPressedEvent : public KeyEvent {
+        public:
+            KeyPressedEvent(int keycode, int repeatCount) 
+                : KeyEvent(keycode), m_RepeatCount(repeatCount) {}
+
+            int getRepeatCount() const {
+                return m_RepeatCount;
+            }
+
+            std::string toString() const override {
+                std::stringstream ss;
+                ss << "KeyPressedEvent: " << m_KeyCode << " (" << m_RepeatCount << " repeats)";
+                return ss.str();
+            }
+
+            static EventType getStaticType() { 
+                return EventType::KeyPressed; 
+            }
+			virtual EventType getEventType() const override { 
+                return GetStaticType(); 
+            }
+			virtual const char* getName() const override { 
+                return "KeyPressed"; 
+            }
+        private:
+            int m_RepeatCount;
+        }
+    }
+}
+
+#endif //_TRENDER_EVENT_KEYPRESSEDEVENT_H_
