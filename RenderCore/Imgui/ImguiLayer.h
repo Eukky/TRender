70a4681159c1ec4ab571a27e939ce0c5b800dd7b
Eukky <uki.sept@gmail.com>
2020-01-25 00:24:34 +0800

    add imgui

---
+++ RenderCore/Imgui/ImguiLayer.h
@@ -0,0 +1,12 @@
+#ifndef _TRENDER_IMGUI_IMGUILAYER_H_
+#define _TRENDER_IMGUI_IMGUILAYER_H_
+
+#include "imgui.h"
+
+namespace TRender {
+    namespace Imgui {
+        
+    }
+}
+
+#endif //_TRENDER_IMGUI_IMGUILAYER_H_
