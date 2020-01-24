c27beb38edfc197a7e7e0cd7125b2db25594165e
Eukky <uki.sept@gmail.com>
2020-01-25 01:17:10 +0800

    update imguilayer

--- RenderCore/Imgui/ImguiLayer.h
+++ RenderCore/Imgui/ImguiLayer.h
@@ -1,11 +1,16 @@
 #ifndef _TRENDER_IMGUI_IMGUILAYER_H_
 #define _TRENDER_IMGUI_IMGUILAYER_H_
 
-#include "imgui.h"
+#include <imgui.h>
+#include "Core/Layer.h"
 
 namespace TRender {
     namespace Imgui {
-        
+        class ImguiLayer : public Core::Layer {
+        public:
+            ImguiLayer() {}
+            ~ImguiLayer() {}
+        };
     }
 }
 
