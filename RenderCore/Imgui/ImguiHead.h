df79adbd23896b3cc423d7f7259cfcf870372f62
Eukky <uki.sept@gmail.com>
2020-01-26 17:30:30 +0800

    add nuklear again

--- RenderCore/Imgui/ImguiHead.h
+++
@@ -1,6 +0,0 @@
-#ifndef _TRENDER_GUI_IMGUIHEAD_H_
-#define _TRENDER_GUI_IMGUIHEAD_H_
-
-#include "ImguiLayer.h"
-
-#endif //_TRENDER_IMGUI_IMGUIHEAD_H_
--- RenderCore/TRender.h
+++ RenderCore/TRender.h
@@ -6,6 +6,6 @@
 #include "Event/EventHead.h"
 #include "Graphics/GraphicsHead.h"
 #include "Maths/MathsHead.h"
-#include "Imgui/ImguiHead.h"
+#include "Gui/GuiHead.h"
 
 #endif //_TRENDER_TRENDER_H_
