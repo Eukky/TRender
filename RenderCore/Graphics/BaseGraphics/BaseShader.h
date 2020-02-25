f8d3560851f68f184137a7d36b11506b99a64147
Eukky <uki.sept@gmail.com>
2020-02-26 00:22:31 +0800

    update shader

--- RenderCore/Graphics/BaseGraphics/BaseShader.h
+++ RenderCore/Graphics/BaseGraphics/BaseShader.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <memory>
+#include <glm/glm.hpp>
 
 namespace TRender {
     namespace Graphics {
@@ -12,6 +13,16 @@ namespace TRender {
 
             virtual void bind() const = 0;
             virtual void unbind() const = 0;
+
+            virtual void setInt(const std::string& name, int value) = 0;
+            virtual void setFloat(const std::string& name, float value) = 0;
+            virtual void setFloat(const std::string& name, const glm::vec1 value) = 0;
+            virtual void setFloat2(const std::string& name, const glm::vec2& value) = 0;
+            virtual void setFloat3(const std::string& name, const glm::vec3& value) = 0;
+            virtual void setFloat4(const std::string& name, const glm::vec4& value) = 0;
+            virtual void setMat2(const std::string& name, const glm::mat2& value) = 0;
+            virtual void setMat3(const std::string& name, const glm::mat3& value) = 0;
+            virtual void setMat4(const std::string& name, const glm::mat4& value) = 0;
 
             virtual const std::string& getName() const = 0;
 
