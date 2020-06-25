c1816a64dec05da5bdd5e66dc03db46d98426f47
Eukky <uki.sept@gmail.com>
2020-06-25 22:56:30 +0800

    精简依赖库

--- RenderCore/Camera/CameraHead.h
+++
@@ -1,6 +0,0 @@
-#ifndef _TRNEDER_CAMERA_CAMERAHEAD_H_
-#define _TRNEDER_CAMERA_CAMERAHEAD_H_
-
-#include "BaseCameraController.h"
-
-#endif //_TRNEDER_CAMERA_CAMERAHEAD_H_
---
+++ include/glm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/wyr/Desktop/TRender/lib/glm/glm
+# Build directory: /Users/wyr/Desktop/TRender/lib/glm/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
